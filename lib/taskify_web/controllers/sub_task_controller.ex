370a9f8d8c58a1a15fcb773eddf0fb3fa160d0fe
Elaine Watanabe <elaine.n.watanabe@gmail.com>
2022-11-08 03:10:49 -0300

    Fixing paths and action arguments

--- lib/taskify_web/controllers/sub_task_controller.ex
+++ lib/taskify_web/controllers/sub_task_controller.ex
@@ -4,17 +4,22 @@ defmodule TaskifyWeb.SubTaskController do
   alias Taskify.SubTasks
   alias Taskify.SubTasks.SubTask
 
-  def index(conn, _params) do
     sub_tasks = SubTasks.list_sub_tasks()
-    render(conn, "index.html", sub_tasks: sub_tasks)
+  def action(conn, _) do
+    args = [conn, conn.params, conn.assigns.current_user]
+    apply(__MODULE__, action_name(conn), args)
   end
 
-  def new(conn, _params) do
+  def index(conn, %{"task_id" => task_id}, current_user) do
+    render(conn, "index.html", sub_tasks: sub_tasks, task_id: task_id)
+  end
+
+  def new(conn, %{"task_id" => task_id}, _current_user) do
     changeset = SubTasks.change_sub_task(%SubTask{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "new.html", changeset: changeset, task_id: task_id)
   end
 
-  def create(conn, %{"sub_task" => sub_task_params}) do
+  def create(conn, %{"task_id" => _task_id, "sub_task" => sub_task_params}, _current_user) do
     case SubTasks.create_sub_task(sub_task_params) do
       {:ok, sub_task} ->
         conn
@@ -26,18 +31,22 @@ defmodule TaskifyWeb.SubTaskController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"task_id" => task_id, "id" => id}, _current_user) do
     sub_task = SubTasks.get_sub_task!(id)
-    render(conn, "show.html", sub_task: sub_task)
+    render(conn, "show.html", sub_task: sub_task, task_id: task_id)
   end
 
-  def edit(conn, %{"id" => id}) do
+  def edit(conn, %{"task_id" => task_id, "id" => id}, _current_user) do
     sub_task = SubTasks.get_sub_task!(id)
     changeset = SubTasks.change_sub_task(sub_task)
-    render(conn, "edit.html", sub_task: sub_task, changeset: changeset)
+    render(conn, "edit.html", sub_task: sub_task, changeset: changeset, task_id: task_id)
   end
 
-  def update(conn, %{"id" => id, "sub_task" => sub_task_params}) do
+  def update(
+        conn,
+        %{"task_id" => task_id, "id" => id, "sub_task" => sub_task_params},
+        _current_user
+      ) do
     sub_task = SubTasks.get_sub_task!(id)
 
     case SubTasks.update_sub_task(sub_task, sub_task_params) do
@@ -47,16 +56,16 @@ defmodule TaskifyWeb.SubTaskController do
         |> redirect(to: Routes.task_sub_task_path(conn, :show, sub_task))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", sub_task: sub_task, changeset: changeset)
+        render(conn, "edit.html", sub_task: sub_task, changeset: changeset, task_id: task_id)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
+  def delete(conn, %{"task_id" => task_id, "id" => id}, _current_user) do
     sub_task = SubTasks.get_sub_task!(id)
     {:ok, _sub_task} = SubTasks.delete_sub_task(sub_task)
 
     conn
     |> put_flash(:info, "Sub task deleted successfully.")
-    |> redirect(to: Routes.task_sub_task_path(conn, :index))
+    |> redirect(to: Routes.task_sub_task_path(conn, :index, task_id))
   end
 end
