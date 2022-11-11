ccd54d95672cf96e45f498c18f3b4e70c3b8d95b
Elaine Watanabe <elaine.n.watanabe@gmail.com>
2022-11-11 01:26:43 -0300

    Refactoring: using a plug to fetch the task from the current_user

--- lib/taskify_web/controllers/sub_task_controller.ex
+++ lib/taskify_web/controllers/sub_task_controller.ex
@@ -4,21 +4,26 @@ defmodule TaskifyWeb.SubTaskController do
   alias Taskify.{SubTasks, Tasks}
   alias Taskify.SubTasks.SubTask
 
+  plug :fetch_task
+
   def action(conn, _) do
-    args = [conn, conn.params, conn.assigns.current_user]
+    args = [conn, conn.params, conn.assigns.task]
     apply(__MODULE__, action_name(conn), args)
   end
 
-  def index(conn, %{"task_id" => task_id}, current_user) do
-    task = Tasks.get_user_task!(current_user, task_id)
+  def index(conn, %{"task_id" => _task_id}, task) do
     sub_tasks = SubTasks.list_sub_tasks(task)
     render(conn, "index.html", sub_tasks: sub_tasks, task: task)
   end
 
-  def new(conn, %{"task_id" => task_id}, current_user) do
-    task = Tasks.get_user_task!(current_user, task_id)
+  def new(conn, %{"task_id" => _task_id}, task) do
     changeset = SubTasks.change_sub_task(%SubTask{})
     render(conn, "new.html", changeset: changeset, task: task)
+  end
+
+  defp fetch_task(conn, _opts) do
+    task = Tasks.get_user_task!(conn.assigns.current_user, conn.params["task_id"])
+    assign(conn, :task, task)
   end
 
 end
