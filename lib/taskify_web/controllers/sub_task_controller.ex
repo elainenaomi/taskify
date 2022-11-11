d5a9b75562ecd5b4dac8d173f843e63f0458e2f7
Elaine Watanabe <elaine.n.watanabe@gmail.com>
2022-11-11 01:22:08 -0300

    Add the new action for SubTaskController
    Also, fixing invalid paths

--- lib/taskify_web/controllers/sub_task_controller.ex
+++ lib/taskify_web/controllers/sub_task_controller.ex
@@ -2,6 +2,7 @@ defmodule TaskifyWeb.SubTaskController do
   use TaskifyWeb, :controller
 
   alias Taskify.{SubTasks, Tasks}
+  alias Taskify.SubTasks.SubTask
 
   def action(conn, _) do
     args = [conn, conn.params, conn.assigns.current_user]
@@ -13,4 +14,11 @@ defmodule TaskifyWeb.SubTaskController do
     sub_tasks = SubTasks.list_sub_tasks(task)
     render(conn, "index.html", sub_tasks: sub_tasks, task: task)
   end
+
+  def new(conn, %{"task_id" => task_id}, current_user) do
+    task = Tasks.get_user_task!(current_user, task_id)
+    changeset = SubTasks.change_sub_task(%SubTask{})
+    render(conn, "new.html", changeset: changeset, task: task)
+  end
+
 end
