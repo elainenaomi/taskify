789071cf95622eb7420ec0edb7b80977f5437579
Elaine Watanabe <elaine.n.watanabe@gmail.com>
2022-11-11 02:19:08 -0300

    Fixing tests for the SubTaskController

--- test/taskify_web/controllers/sub_task_controller_test.exs
+++ test/taskify_web/controllers/sub_task_controller_test.exs
@@ -3,37 +3,44 @@ defmodule TaskifyWeb.SubTaskControllerTest do
 
   import Taskify.SubTasksFixtures
 
+  setup [:register_and_log_in_user, :create_task]
+
   @create_attrs %{completed: true, description: "some description", name: "some name"}
-  @update_attrs %{completed: false, description: "some updated description", name: "some updated name"}
+  @update_attrs %{
+    completed: false,
+    description: "some updated description",
+    name: "some updated name"
+  }
   @invalid_attrs %{completed: nil, description: nil, name: nil}
 
   describe "index" do
-    test "lists all sub_tasks", %{conn: conn} do
-      conn = get(conn, Routes.sub_task_path(conn, :index))
+    test "lists all sub_tasks", %{conn: conn, task: task} do
+      conn = get(conn, Routes.task_sub_task_path(conn, :index, task))
       assert html_response(conn, 200) =~ "Listing Sub tasks"
     end
   end
 
   describe "new sub_task" do
-    test "renders form", %{conn: conn} do
-      conn = get(conn, Routes.sub_task_path(conn, :new))
+    test "renders form", %{conn: conn, task: task} do
+      conn = get(conn, Routes.task_sub_task_path(conn, :new, task))
       assert html_response(conn, 200) =~ "New Sub task"
     end
   end
 
   describe "create sub_task" do
-    test "redirects to show when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.sub_task_path(conn, :create), sub_task: @create_attrs)
+    test "redirects to show when data is valid", %{conn: conn, task: task} do
+      conn = post(conn, Routes.task_sub_task_path(conn, :create, task), sub_task: @create_attrs)
 
       assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == Routes.sub_task_path(conn, :show, id)
+      assert redirected_to(conn) == Routes.task_sub_task_path(conn, :show, task, id)
 
-      conn = get(conn, Routes.sub_task_path(conn, :show, id))
+      conn = get(conn, Routes.task_sub_task_path(conn, :show, task, id))
       assert html_response(conn, 200) =~ "Show Sub task"
     end
 
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.sub_task_path(conn, :create), sub_task: @invalid_attrs)
+    test "renders errors when data is invalid", %{conn: conn, task: task} do
+      conn = post(conn, Routes.task_sub_task_path(conn, :create, task), sub_task: @invalid_attrs)
+
       assert html_response(conn, 200) =~ "New Sub task"
     end
   end
@@ -41,8 +48,8 @@ defmodule TaskifyWeb.SubTaskControllerTest do
   describe "edit sub_task" do
     setup [:create_sub_task]
 
-    test "renders form for editing chosen sub_task", %{conn: conn, sub_task: sub_task} do
-      conn = get(conn, Routes.sub_task_path(conn, :edit, sub_task))
+    test "renders form for editing chosen sub_task", %{conn: conn, sub_task: sub_task, task: task} do
+      conn = get(conn, Routes.task_sub_task_path(conn, :edit, task, sub_task))
       assert html_response(conn, 200) =~ "Edit Sub task"
     end
   end
@@ -50,16 +57,24 @@ defmodule TaskifyWeb.SubTaskControllerTest do
   describe "update sub_task" do
     setup [:create_sub_task]
 
-    test "redirects when data is valid", %{conn: conn, sub_task: sub_task} do
-      conn = put(conn, Routes.sub_task_path(conn, :update, sub_task), sub_task: @update_attrs)
-      assert redirected_to(conn) == Routes.sub_task_path(conn, :show, sub_task)
+    test "redirects when data is valid", %{conn: conn, sub_task: sub_task, task: task} do
+      conn =
+        put(conn, Routes.task_sub_task_path(conn, :update, task, sub_task),
+          sub_task: @update_attrs
+        )
 
-      conn = get(conn, Routes.sub_task_path(conn, :show, sub_task))
+      assert redirected_to(conn) == Routes.task_sub_task_path(conn, :show, task, sub_task)
+
+      conn = get(conn, Routes.task_sub_task_path(conn, :show, task, sub_task))
       assert html_response(conn, 200) =~ "some updated description"
     end
 
-    test "renders errors when data is invalid", %{conn: conn, sub_task: sub_task} do
-      conn = put(conn, Routes.sub_task_path(conn, :update, sub_task), sub_task: @invalid_attrs)
+    test "renders errors when data is invalid", %{conn: conn, sub_task: sub_task, task: task} do
+      conn =
+        put(conn, Routes.task_sub_task_path(conn, :update, task, sub_task),
+          sub_task: @invalid_attrs
+        )
+
       assert html_response(conn, 200) =~ "Edit Sub task"
     end
   end
@@ -67,18 +82,23 @@ defmodule TaskifyWeb.SubTaskControllerTest do
   describe "delete sub_task" do
     setup [:create_sub_task]
 
-    test "deletes chosen sub_task", %{conn: conn, sub_task: sub_task} do
-      conn = delete(conn, Routes.sub_task_path(conn, :delete, sub_task))
-      assert redirected_to(conn) == Routes.sub_task_path(conn, :index)
+    test "deletes chosen sub_task", %{conn: conn, sub_task: sub_task, task: task} do
+      conn = delete(conn, Routes.task_sub_task_path(conn, :delete, task, sub_task))
+      assert redirected_to(conn) == Routes.task_sub_task_path(conn, :index, task)
 
       assert_error_sent 404, fn ->
-        get(conn, Routes.sub_task_path(conn, :show, sub_task))
+        get(conn, Routes.task_sub_task_path(conn, :show, task, sub_task))
       end
     end
   end
 
-  defp create_sub_task(_) do
-    sub_task = sub_task_fixture()
+  defp create_task(%{user: user}) do
+    task = Taskify.TasksFixtures.task_fixture(user)
+    %{task: task}
+  end
+
+  defp create_sub_task(%{task: task}) do
+    sub_task = sub_task_fixture(task)
     %{sub_task: sub_task}
   end
 end
