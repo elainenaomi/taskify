8d3094b7c6ccc30791086a922f9c427c4fccf093
Elaine Watanabe <elaine.n.watanabe@gmail.com>
2022-11-11 01:30:01 -0300

    Add create and show actions to SubTaskController

--- lib/taskify_web/controllers/sub_task_controller.ex
+++ lib/taskify_web/controllers/sub_task_controller.ex
@@ -21,6 +21,23 @@ defmodule TaskifyWeb.SubTaskController do
     render(conn, "new.html", changeset: changeset, task: task)
   end
 
+  def create(conn, %{"task_id" => _task_id, "sub_task" => sub_task_params}, task) do
+    case SubTasks.create_sub_task(task, sub_task_params) do
+      {:ok, sub_task} ->
+        conn
+        |> put_flash(:info, "Sub task created successfully.")
+        |> redirect(to: Routes.task_sub_task_path(conn, :show, task, sub_task))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset, task: task)
+    end
+  end
+
+  def show(conn, %{"task_id" => _task_id, "id" => id}, task) do
+    sub_task = SubTasks.get_sub_task!(task, id)
+    render(conn, "show.html", sub_task: sub_task, task: task)
+  end
+
   defp fetch_task(conn, _opts) do
     task = Tasks.get_user_task!(conn.assigns.current_user, conn.params["task_id"])
     assign(conn, :task, task)
